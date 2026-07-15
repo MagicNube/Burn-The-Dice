2a38ce965515a3e24989252964bf0ed2df4feadd
Hugo Juan Gomez <hugojuan09082004@gmail.com>
2026-07-15 15:05:32 +0200

    Cartas en abanico y uso de anchor points para la interfaz de las cartas

--- modules/cards/scripts/card.gd
+++ modules/cards/scripts/card.gd
@@ -1,45 +1,42 @@
 class_name Card
-extends TextureRect
+extends Control
+
+@export var visual_texture: TextureRect
 
 # Datos de la carta
 var data: CardBlueprint
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
+# Funcion para inicializar la carta
 func setup(card_data: CardBlueprint) -> void:
 	data = card_data
-	texture = card_data.image
+	visual_texture.texture = card_data.image 
 
 func _get_drag_data(at_position: Vector2) -> Variant:
-	# Creamos envoltorio de la carta para que se centre al cursor al agarrar
+	# Creamos el wrapper vacío para el centro del ratón
 	var preview_wrapper = Control.new()
 	
-	# Creamos carta fantasma mientras se agarra la carta con configuración igual a la real
+	# Creamos la textura fantasma y la configuramos igual que la original
 	var preview = TextureRect.new()
 	preview.texture = data.image
 	preview.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	preview.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
-	preview.size = Vector2(120.0, 180.0)
+	preview.size = visual_texture.size 
 	
-	# Centramos en el cursor
+	# La desplazamos para que el ratón quede en el medio
 	preview.position = -preview.size / 2.0
 	
-	# Añadimos la carta fantasma al envoltorio
+	# Lo empaquetamos todo
 	preview_wrapper.add_child(preview)
-	
-	# Mostramos el envoltorio con la carta fantasma, asi la carta se vera centrada en el raton
 	set_drag_preview(preview_wrapper)
 	
-	# Hacemos la carta original translucida
+	# Volvemos translúcida la carta original en la mano
 	modulate = Color(1, 1, 1, 0.5)
 	
-	# Devolvemos los datos reales
 	return self
 
-# Funcion que recibe automaticamente todas las señales de la carta
-# Comprueba si la notificacion es que la carta se ha soltado y en ese caso restaura la opacidad original
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_DRAG_END:
 		modulate = Color(1, 1, 1, 1)
--- modules/cards/scripts/visual_hand.gd
+++ modules/cards/scripts/visual_hand.gd
@@ -3,9 +3,12 @@ extends HBoxContainer
 # Referencia a la logica de la baraja
 @export var deck_logic: Node
 
-# Called when the node enters the scene tree for the first time.
+var base_angle: float = 5.0
+
+# Conectamos señales de creacion de cartas y poner las cartas en abanico
 func _ready() -> void:
 	deck_logic.card_drawn.connect(on_card_drawn)
+	child_order_changed.connect(recalculate_fan)
 
 # Funcion para la señal card_drawn precarga una escena de tipo Card, 
 # la inicializa con los datos de la carta robada y la añade al arbol de la escena
@@ -13,3 +16,18 @@ func on_card_drawn(card_data: CardBlueprint) -> void:
 	var new_card = preload("res://modules/cards/scenes/card.tscn").instantiate()
 	new_card.setup(card_data)
 	add_child(new_card)
+
+func recalculate_fan() -> void:
+	var cards = get_children()
+	var total_cards = cards.size()
+	
+	if total_cards == 0:
+		return
+	
+	var center_index = (total_cards - 1) / 2.0
+	var vertical_drop = 5.0
+	
+	for i in total_cards:
+		var distance_from_center: float =  i - center_index
+		cards[i].visual_texture.rotation_degrees = distance_from_center * base_angle
+		cards[i].visual_texture.position.y = (distance_from_center * distance_from_center) * vertical_drop
